48aeed64fb1557f6606e6d5138fa1e66ccc862f0
eisenspiel <eisenspiel@gmail.com>
2021-03-14 00:16:01 +0300

    Add project files.

---
+++ WordsCountingLib/WordsCountingClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace WordsCountingLib
+{
+    public class WordsCountingClass
+    {
+        private Dictionary<string, int> CountWords(string TextInput)
+        {
+            //Засекаем время обработки в одном потоке
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            // Заменяем все символы, которые не буквы, на пробелы и переводим текст в нижний регистр
+            TextInput = new Regex("[^a-zA-Zа-яёА-ЯЁ]").Replace(TextInput, " ").ToLower();
+
+            // Создаём массив полученных слов
+            var words = TextInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Создаём словарь для пары "слово - количество"
+            Dictionary<string, int> wordCountUnsorted = new Dictionary<string, int>();
+
+            // Добавляем слово из массива в словарь, если его ещё нет. Если оно уже есть, увеличиваем количество на единицу
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!wordCountUnsorted.ContainsKey(words[i]))
+                {
+                    wordCountUnsorted.Add(words[i], 1);
+                }
+                else
+                {
+                    wordCountUnsorted[words[i]] += 1;
+                }
+            }
+
+            //Сортируем по убыванию вхождений
+            var sortedDict = (from entry in wordCountUnsorted orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            //выводим время обработки
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:000}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds);
+            Console.WriteLine($"The text processing by one thread took {elapsedTime}");
+
+
+            return (Dictionary<string, int>)sortedDict;
+        }
+
+        public Dictionary<string, int> CountWordsMultithread(string TextInput)
+        {
+            //Засекаем время обработки в одном потоке
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            // Заменяем все символы, которые не буквы, на пробелы и переводим текст в нижний регистр
+            TextInput = new Regex("[^a-zA-Zа-яёА-ЯЁ]").Replace(TextInput, " ").ToLower();
+
+            // Создаём массив полученных слов
+            var words = TextInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Создаём словарь для пары "слово - количество", который может работать с несколькими потоками
+            ConcurrentDictionary<string, int> cd = new ConcurrentDictionary<string, int>();
+
+            // Добавляем слово из массива в словарь, и единицу в значение, если его ещё нет. Если слово уже есть, увеличиваем количество на единицу
+            Parallel.For(0, words.Length, i => {
+                cd.AddOrUpdate(words[i], 1, (key, oldValue) => oldValue + 1);
+            });
+
+            //Сортируем по убыванию вхождений
+            //var sortedDict = (from entry in cd.AsParallel() orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+            var sortedDict = (from entry in cd orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            //выводим время обработки
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:000}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds);
+            Console.WriteLine($"The text processing by multiple threads in a public metod took {elapsedTime}");
+
+            return (Dictionary<string, int>)sortedDict;
+        }
+    }
+}
---
+++ words_net48/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using WordsCountingLib;
+using System.Reflection;
+
+namespace words_net48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запрашиваем путь к файлу, если путь не введён, по умолчанию путь к файлу в корне диска D:
+            Console.Title = "Words Counting -- Test Task for Digital Design";
+            Console.WriteLine("Please enter the path to your txt file\nor\nJust press Enter if you want to use the default path \"D:\\Vojnaimir.Tom1.utf8.txt\"");
+            string path = Console.ReadLine();
+            if (path == "") path = @"D:\Vojnaimir.Tom1.UTF8.txt";
+
+
+            //Читаем файл
+            Console.WriteLine($"Trying to open {path}...");
+            string whole_text = "";
+            try
+            {
+                whole_text = System.IO.File.ReadAllText(path);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something wrong with the path you entered. Please start again and enter a valid vath");
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"Done! There are {whole_text.Length} symbols.\nNow please wait, the book is big, so it's gonna take a while...");
+            
+            //Создаём словарь, куда будем записывать результат подсчёта слов
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            //Предлагаем выбрать, как считать слова: 1 - приватным методоном в одном потоке, 2 - публичным методом многопоточно, 3 - на сервере многопоточно
+            Console.WriteLine("\nPlease enter 1 if you want to use private metod with one thread\nor" +
+                "\nEnter 2 for public method with multiple threads\nor" +
+                "\nEnter anything other for counting words on the server side");
+            string ChosenMethod = Console.ReadLine();            
+            if (ChosenMethod == "1")
+            {
+                //Вызываем из библиотеки WordsCountLib приватный метод подсчёта слов из текста
+                var WordsCountIstance = new WordsCountingClass();
+                var magicMethod = WordsCountIstance.GetType().GetMethod("CountWords", BindingFlags.NonPublic | BindingFlags.Instance);
+                wordCount = (Dictionary<string, int>)magicMethod.Invoke(WordsCountIstance, new object[] { whole_text });
+            }
+            else if (ChosenMethod == "2")
+            {
+                //Вызываем из библиотеки WordsCountLib публичный метод подсчёта слов из текста с многопоточностью
+                var WordsCountIstance = new WordsCountingClass();
+                wordCount = WordsCountIstance.CountWordsMultithread(whole_text);
+            }
+            else
+            {
+                //Отправляем текст для подсчёта на веб-сервер
+                wordCount = CountWordsWCF(whole_text);
+            }
+
+
+            // Выводим топ-10 слов
+            printTop(wordCount, 7);
+
+
+            //Записываем результат в файл
+            Console.WriteLine("Now please enter the path to the folder where you want to save the results.\nor\nJust press Enter if you want to use the default path \"D:\\\"");
+            path = Console.ReadLine();
+            if (path == "") path = @"D:\";
+            Console.WriteLine($"Full results are available in a txt file here: {path}\\results.txt");
+            File.WriteAllLines($"{path}\\results.txt", wordCount.Select(x => x.Key + " " + x.Value));
+
+            // Ждём реакции пользователя
+            Console.WriteLine("Press any key to close the console app");
+            Console.ReadKey();
+
+        }
+
+       /* 
+        private static Dictionary<string, int> CountWordsWCF(string text)
+        {
+            ServiceReference1.CompositeType ret = null;
+            Dictionary<string, int> dict;
+            using (var client = new ServiceReference1.Service1Client())
+            {
+                ret = client.GetDataUsingDataContract(new ServiceReference1.CompositeType
+                {
+                    BoolValue = true,
+                    StringValue = text
+                });
+            }
+            dict = ret.Dict;
+            return dict;
+        }*/
+        
+        
+        private static Dictionary<string, int> CountWordsWCF(string text)
+        {
+            Dictionary<string, int> dict;
+            using (var client = new ServiceReference1.Service1Client())
+            {
+                dict = client.GetData(text);
+            }
+            return dict;
+        }
+        
+
+        private static void printTop(Dictionary<string, int> dict, int n = 5)
+        {
+            Console.WriteLine($"Here's top {n} most frequent words from the book:");
+            int count = 0;
+            foreach (var item in dict)
+            {
+                Console.WriteLine("{0}   {1}", item.Key, item.Value);
+                count++;
+                if (count == n) break;
+            }
+        }
+    }
+}
